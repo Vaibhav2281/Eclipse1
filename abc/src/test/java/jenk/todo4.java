c911d79b3f52cc74990ea4e937df06a679d06fe2
vaibhav <vaibhav.badheka@gmail.com>
2024-07-23 08:44:39 +0530

    try

---
+++ abc/src/test/java/jenk/todo4.java
@@ -0,0 +1,88 @@
+package jenk;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.junit.*;
+
+public class todo4 {
+
+	private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeClass
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        System.setProperty("webdriver.edgedriver.driver", "D:\\Edge Driver\\msedgedriver.exe");
+        driver = new EdgeDriver();
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+        String spanText;
+        System.out.println("Loading Url");
+
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li1")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li2")).click();
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li3")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li4")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
+        driver.findElement(By.id("addbutton")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li1")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li3")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li7")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li8")).click();
+
+        System.out.println("Entering Text");
+        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
+
+        driver.findElement(By.id("addbutton")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li9")).click();
+
+        // Let's also assert that the todo we added is present in the list.
+
+        spanText = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[9]/span")).getText();
+        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
+        Status = "passed";
+        Thread.sleep(150);
+
+        System.out.println("TestFinished");
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
